f8c1c55fd062b0c9cca6cbff3710feaa044fdc2c
xiaojie45 <418022698@qq.com>
2022-03-26 12:16:36 +0800

    harry potter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { Suspense, useLayoutEffect } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls, useGLTF } from '@react-three/drei'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Canvas shadows camera={{ position: [10, 4, 0 ], near: 0.1, far: 50 }}>
+      <ambientLight intensity={0.8} />
+      <spotLight angle={0.2} color="#ffd0d0" penumbra={1} position={[60, 60, -40]} shadow-mapSize={[2048, 2048]} shadow-bias={-0.0001} castShadow />
+      <Suspense fallback={null}>
+          <Harrypotter scale={0.5} position={[0, 0, 0]} />
+      </Suspense>
+      <OrbitControls
+        enableZoom={false}
+        enablePan={false}
+        minAzimuthAngle={Math.PI / 3}
+        maxAzimuthAngle={Math.PI / 1.5}
+        maxPolarAngle={Math.PI / 2.5}
+        minPolarAngle={Math.PI / 2.5}
+        screenSpacePanning={false}
+        rotateSpeed={0.5}
+      />
+    </Canvas>
+  )
 }
 
-export default App;
+function Harrypotter({ ...props }) {
+  const { scene, nodes } = useGLTF('/harrypotter.glb')
+  useLayoutEffect(() => Object.values(nodes).forEach((node) => (node.receiveShadow = node.castShadow = true)))
+  useFrame((state, delta) => {
+    state.camera.lookAt(0, 0, 0)
+  })
+  return <primitive object={scene} {...props} />
+}
+
+
+useGLTF.preload('/harrypotter.glb')
